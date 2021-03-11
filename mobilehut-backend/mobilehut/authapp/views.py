320b0a8495835837140e7a931afaebf1c427879b
nabeel <nabeelayub@hotmail.com>
2021-03-11 19:37:09 +0500

    account ui and backend done

--- mobilehut-backend/mobilehut/authapp/views.py
+++ mobilehut-backend/mobilehut/authapp/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializer import UserSerializer,UserUpdateSerializer,CustomTokenObtainPairSerializer
 from .models import User
+from Orders.models import Order,ProductOrder
 from rest_framework import status
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -65,3 +66,37 @@ class ManageUser(APIView):
 class CustomTokenObtainPairView(TokenObtainPairView):  # to add token and 
     # Replace the serializer with your custom
     serializer_class = CustomTokenObtainPairSerializer
+
+
+class AccountDetail(APIView):
+
+    def post(self,request):
+
+        email=request.data.get('email')
+        user=User.objects.get(email=email)
+        order=Order.objects.filter(user=user).prefetch_related('productorder_set')
+        order_details=[]  # order details
+        product_details=[]
+      
+
+        index=0
+        for order in order:
+            order_details.append({"ordernumber":order.id,"orderdate":order.order_date,"orderstatus":order.order_status})
+            for order in order.productorder_set.all():
+                product_details.append({"name":order.product.product_name,"quantity":order.quantity,"model":order.modelP,"colour":order.colour})
+            order_details[index]['product']=product_details
+            product_details=[]
+            index=index+1
+        
+        user_address=[]
+        user_address.append({"phonenumber":user.phonenumber,"address":user.address,"city":user.city,"province":user.province})
+        
+        user_account=[]
+        user_account.append({"name":user.username,"email":user.email})
+
+        account_details={}
+        account_details['order']=order_details
+        account_details['address']=user_address
+        account_details['account']=user_account
+
+        return Response(account_details,status=status.HTTP_200_OK)
